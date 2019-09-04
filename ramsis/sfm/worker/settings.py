95f1f9d1d1be9b419c7a6ea12bd2c684bd5936c0
sarsonl <laura.sarson@sed.ethz.ch>
2019-09-04 18:08:01 +0200

    Changing config back to original setup

---
+++ ramsis/sfm/worker/settings.py
@@ -0,0 +1,14 @@
+# Copyright 2019, ETH Zurich - Swiss Seismological Service SED
+"""
+General purpose configuration constants.
+"""
+
+# -----------------------------------------------------------------------------
+# General ramsis.sfm.worker configuration
+# -----------------------------------------------------------------------------
+PATH_RAMSIS_WORKER_CONFIG = '/path/to/ramsis_config'
+# worker resource URL path
+PATH_RAMSIS_WORKER_SCENARIOS = '/runs'
+
+# -----------------------------------------------------------------------------
+RAMSIS_WORKER_DB_CONFIG_SECTION = 'CONFIG_WORKER_DB'
